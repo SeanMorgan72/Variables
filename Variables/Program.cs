6225a73e4a3d0f587411f4a408271eef9c6a9fab
Sean Morgan <scm0323@att.net>
2021-08-19 14:47:34 -0500

    added variables program

---
+++ Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Chris Bradley";
+            int year = 2021;
+            char letter = 'Y';
+            bool isFunny = true;
+            double rate = 7.5;
+            decimal pay = 75.00m;
+
+            Console.WriteLine($"{name}, who joined TrueCoders in {year}; reasons {letter}, who knows. " +
+                $"{isFunny}, he is funny; though his humor is on a rate of {rate} on the rediculous scale. " +
+                $"Though, he does pay each of us {pay} par hour to laugh at his jokes.");
+        }
+    }
+}
